a69b1fc15d68734e0434cd8fdb9baaaf11906c5f
kimjinsoo <roorooro9933@gmail.com>
2021-03-18 13:40:45 +0900

    handling reply like request

--- controller/reply.js
+++ controller/reply.js
@@ -44,31 +44,30 @@ exports.createReply = (req, res, next) => {
       }
       next(error);
     });
+};
 
-  // const reply = new Reply({
-  //   userId: userId,
-  //   feedId: "604f2a68eb32b80d40a8eb6b",
-  //   content: content,
-  //   likeCount: 2,
-  // });
+exports.createLike = (req, res, next) => {
+  const userId = req.userId;
+  const replyId = req.body.replyId;
 
-  // reply
-  //   .save()
-  //   .then((reply) => {
-  //     myReply = reply;
-  //     return Feed.findById({ _id: "604f2a68eb32b80d40a8eb6b" });
-  //   })
-  //   .then((feed) => {
-  //     feed.replyIds.push(myReply._id.toString());
-  //     return feed.save();
-  //   })
-  //   .then((result) => {
-  //     res.json({ message: "reply add success" });
-  //   })
-  //   .catch((error) => {
-  //     if (!error.statusCode) {
-  //       error.statusCode = 500;
-  //     }
-  //     next(error);
-  //   });
+  Reply.findOne({ _id: replyId })
+    .then((reply) => {
+      if (!reply) {
+        const error = new Error("reply not found.");
+        error.statusCode = 500;
+        throw error;
+      }
+
+      reply.likeCount++;
+      return reply.save();
+    })
+    .then((reply) => {
+      res.json({ message: "reply like increase success" });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
 };
--- routes/reply.js
+++ routes/reply.js
@@ -8,4 +8,6 @@ const isAuth = require("../middleware/isAuth");
 
 router.post("/", isAuth, replyController.createReply);
 
+router.post("/like", isAuth, replyController.createLike);
+
 module.exports = router;
