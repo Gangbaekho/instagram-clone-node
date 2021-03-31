6e159575103827056b313e71fc50172c1bf001ed
kimjinsoo <roorooro9933@gmail.com>
2021-03-31 12:31:46 +0900

    socket io setting

--- controller/activitiy.js
+++ controller/activitiy.js
@@ -19,22 +19,3 @@ exports.getActivities = (req, res, next) => {
       next(error);
     });
 };
-
-// exports.test = (req, res, next) => {
-//   const userId = req.userId;
-
-//   Activity.find({ whomId: userId })
-//     .sort({ _id: -1 })
-//     .limit(20)
-//     .populate("feedId")
-//     .populate("replyId")
-//     .then((activities) => {
-//       res.json({ message: "succes", activities: activities });
-//     })
-//     .catch((error) => {
-//       if (!error.statusCode) {
-//         error.statusCode = 500;
-//       }
-//       next(error);
-//     });
-// };
